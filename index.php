d0885de404f24227f9616b96cf4ed5578b18dcef
pcruz <pedro.cruz@log.pt>
2014-07-09 18:28:25 +0100

    Change to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7 ui-mobile-rendering" lang="en"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8 ui-mobile-rendering" lang="en"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9 ui-mobile-rendering" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js ui-mobile-rendering" lang="en"> <!--<![endif]-->
+<?php wp_head() ?>
+<body>
+  <div data-role="page">
+    <header></header>
+    <div id="main" class="container-fluid"></div>
+    <footer class="footer navbar navbar-fixed-bottom"></footer>
+  </div>
+</body>
+</html>
