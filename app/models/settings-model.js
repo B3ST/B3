4518cabd5f7ab816f0184aa16d080a8fe775d03d
pcruz <pedro.cruz@log.pt>
2014-07-11 11:34:51 +0100

    Minor change in settings model

--- app/models/settings-model.js
+++ app/models/settings-model.js
@@ -2,12 +2,6 @@ define([
   'jquery',
   'backbone'
 ], function ($, Backbone) {
-  var Settings = Backbone.Model.extend({
-    defaults: {
-      url   : WP_API_SETTINGS.url,
-      nonce : WP_API_SETTINGS.nonce
-    }
-  });
-
-  return new Settings();
+  var Settings = Backbone.Model.extend({});
+  return new Settings(WP_API_SETTINGS);
 });
