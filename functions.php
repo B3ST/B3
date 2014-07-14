177f60ed0cb8a97013b89e00641fd28386e39623
Luis Rodrigues <luis.rodrigues@log.pt>
2014-07-14 13:45:53 +0100

    Loader WP

--- functions.php
+++ functions.php
@@ -1,34 +1,158 @@
 <?php
+/**
+ * B3 theme logic.
+ */
 
-function add_require() {
-  $root_url = get_stylesheet_directory_uri();
-  $settings = array(
-    'root'  => $root_url,
-    'url'   => home_url(json_get_url_prefix()),
-    'name'  => get_bloginfo('name'),
-    'nonce' => wp_create_nonce('wp_json'));
-
-  wp_register_script('requirejs', $root_url . '/libs/require.js');
-  wp_localize_script('requirejs', 'WP_API_SETTINGS', $settings);
-  wp_enqueue_script('requirejs', array(), time(), false);
+if (!defined( 'WPINC' )) {
+    die;
 }
-add_action('wp_enqueue_scripts', 'add_require', 11);
 
-function add_data_main($src, $handle){
-  if ($handle != 'requirejs')
-    return $src;
+class B3Theme {
 
-  return $src . '" id="requirejs-loader" data-main="' . get_stylesheet_directory_uri() . '/app/config/init.js"';
+    /**
+     * Theme slug.
+     * @var [type]
+     */
+    protected $slug = 'b3';
+
+    /**
+     * [$require description]
+     * @var string
+     */
+    protected $require_uri = '';
+
+    /**
+     * [$loader description]
+     * @var string
+     */
+    protected $loader_uri = '';
+
+    /**
+     * [__construct description]
+     */
+    public function __construct () {
+
+        $this->settings_uri = get_template_directory_uri() . '/settings.js';
+
+        // $this->require_uri = get_template_directory_uri() . '/lib/require/build/require.min.js';
+        $this->require_uri  = get_template_directory_uri() . '/libs/require.js';
+
+        $this->loader_uri   = defined( 'WP_DEBUG' ) && WP_DEBUG
+                            ? get_template_directory_uri() . '/app/config/init.js'
+                            : get_template_directory_uri() . '/dist/config/init.js'
+                            ;
+
+        $this->setup();
+    }
+
+    /**
+     * [is_wp_api_active description]
+     * @return boolean [description]
+     */
+    protected function is_wp_api_active () {
+        return function_exists( 'json_get_url_prefix' );
+    }
+
+    /**
+     * [is_wp_api_active description]
+     * @return boolean [description]
+     */
+    public function wp_api_check () {
+        if (!$this->is_wp_api_active()) {
+            wp_die( __( 'The WordPress API is unavailable. Please install and enable the WP API plugin to use this theme.', 'b3' ),
+                __( 'Error: WP API Unavailable', 'b3' ) );
+        }
+    }
+
+    /**
+     * [setup description]
+     */
+    public function setup () {
+        load_theme_textdomain( $this->slug, get_template_directory() . '/languages' );
+
+        $this->setup_menus();
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 'html5', array(
+            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+        ) );
+
+        add_action( 'widgets_init'      , array( $this, 'setup_widgets' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'setup_scripts' ) );
+        add_action( 'wp_head'           , array( $this, 'enqueue_require_script' ), 20, 0 );
+    }
+
+    /**
+     * [register_menus description]
+     */
+    protected function setup_menus () {
+        register_nav_menus( array(
+            'primary' => __( 'Primary Menu', 'b3' ),
+        ) );
+    }
+
+    /**
+     * [register_widgets description]
+     * @return [type] [description]
+     */
+    public function setup_widgets () {
+        register_sidebar( array(
+            'name'          => __( 'Sidebar', 'b3' ),
+            'id'            => 'sidebar',
+            'description'   => '',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h1 class="title">',
+            'after_title'   => '</h1>',
+        ) );
+    }
+
+    /**
+     * Inject client application scripts.
+     *
+     * This action is called on `wp_enqueue_scripts` and injects required client
+     * application settings from the backend, such as:
+     *
+     * - `root`:  Theme root URI.
+     * - `url`:   RESTful WP API endpoint prefix.
+     * - `name`:  Site name.
+     * - `nonce`: Nonce string.
+     */
+    public function setup_scripts () {
+        if (!$this->is_wp_api_active()) {
+            return;
+        }
+
+        $settings = array(
+            'root'  => get_stylesheet_directory_uri(),
+            'url'   => home_url( json_get_url_prefix() ),
+            'name'  => get_bloginfo( 'name' ),
+            'nonce' => wp_create_nonce( 'wp_json' )
+            );
+
+        wp_register_script( $this->slug . '-settings', $this->settings_uri );
+        wp_localize_script( $this->slug . '-settings', 'WP_API_SETTINGS', $settings );
+        wp_enqueue_script( $this->slug . '-settings' );
+    }
+
+    public function enqueue_require_script () {
+        if (!$this->is_wp_api_active()) {
+            return;
+        }
+        echo '<script src="' . $this->require_uri . '" data-main="' . $this->loader_uri . '"></script>';
+    }
+
 }
-add_filter('script_loader_src', 'add_data_main', 10, 2);
 
-function unclean_url($good_protocol_url, $original_url, $_context){
-  if (strpos($original_url, 'requirejs-loader') !== false){
-    remove_filter('clean_url', 'unclean_url', 10, 3);
-    $url_parts = parse_url($good_protocol_url);
-    return $url_parts['scheme'] . '://' . $url_parts['host'] . ':' . $url_parts['port'] . $url_parts['path'] . "' data-main='" . get_stylesheet_directory_uri() . "/app/config/init.js'";
-  }
+function B3 () {
+    global $GLOBALS;
 
-  return $good_protocol_url;
+    if (!isset( $GLOBALS['b3'] )) {
+        $GLOBALS['b3'] = new B3Theme();
+    }
+
+    return $GLOBALS['b3'];
 }
-add_filter('clean_url', 'unclean_url', 10, 3);
+
+add_action( 'after_setup_theme', 'B3' );
--- index.php
+++ index.php
@@ -1,19 +1,26 @@
-<!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!DOCTYPE html>
 <!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7 ui-mobile-rendering" lang="en"> <![endif]-->
+<html class="no-js lt-ie9 lt-ie8 lt-ie7 ui-mobile-rendering" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8 ui-mobile-rendering" lang="en"> <![endif]-->
+<html class="no-js lt-ie9 lt-ie8 ui-mobile-rendering" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>
-<html class="no-js lt-ie9 ui-mobile-rendering" lang="en"> <![endif]-->
+<html class="no-js lt-ie9 ui-mobile-rendering" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js ui-mobile-rendering" lang="en"> <!--<![endif]-->
-<?php wp_head() ?>
-<body>
-  <div data-role="page">
-    <header></header>
-    <div id="main" class="container-fluid"></div>
-    <footer class="footer navbar navbar-fixed-bottom"></footer>
-  </div>
+<html class="no-js ui-mobile-rendering" <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page" data-role="page">
+        <header></header>
+        <div id="main"></div>
+        <footer></footer>
+    </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
