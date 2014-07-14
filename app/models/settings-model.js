75533e1428f9b50e5a624703cd0baebf1fb45b7c
pcruz <pedro.cruz@log.pt>
2014-07-14 16:30:01 +0100

    Minor change

--- app/models/settings-model.js
+++ app/models/settings-model.js
@@ -5,7 +5,8 @@ define([
   var Settings = Backbone.Model.extend({
     defaults: {
       url   : WP_API_SETTINGS.url,
-      nonce : WP_API_SETTINGS.nonce
+      nonce : WP_API_SETTINGS.nonce,
+      name  : WP_API_SETTINGS.name
     }
   });
 
