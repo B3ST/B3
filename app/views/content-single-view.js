7dcd0e966fc43952ce38d07abdd638d73f1be2a5
pcruz <pedro.cruz@log.pt>
2014-07-18 16:39:15 +0100

    Add comment section and reply section
    Note to self: TURN OFF THE COMPLICATO-METER !!

--- app/views/content-single-view.js
+++ app/views/content-single-view.js
@@ -1,3 +1,5 @@
+'use strict';
+
 define([
   'jquery',
   'underscore',
@@ -9,9 +11,8 @@ define([
   'content/post-template'
 ], function ($, _, Marionette, dust, dustMarionette, CommentView) {
   var ContentSingleView = Backbone.Marionette.CompositeView.extend({
-    template:          'content/post-template.dust',
-    childView:          CommentView,
-    childViewContainer: '#b3-comments',
+    template:  'content/post-template.dust',
+    childView: CommentView,
 
     serializeData: function () {
       return _.extend(this.model.toJSON(), {b3type: 'single'});
@@ -22,6 +23,15 @@ define([
         done: function (data) { this.collection.add(data.models); }.bind(this),
         fail: function () { this.displayError(); }.bind(this)
       });
+    },
+
+    attachHtml: function (collectionView, itemView, index) {
+      if (itemView.model.get('parent') > 0) {
+        collectionView.$('#comment-' + itemView.model.get('parent') + ' > ul.b3-comments').append(itemView.el);
+      } else {
+        var commentSection = collectionView.$('.b3-comments');
+        $(commentSection[0]).append(itemView.el);
+      }
     },
 
     displayError: function () {
--- test/jasmine/specs/core/views/content-single-view.spec.js
+++ test/jasmine/specs/core/views/content-single-view.spec.js
@@ -52,8 +52,43 @@ define([
             this.view.render();
 
             this.server.respond();
-            expect(this.view.$('#b3-comments').children().length).toEqual(2);
+            expect(this.view.$('.b3-comments').children().length).toEqual(2);
             expect(this.view.$('.b3-comment-content').length).toEqual(2);
+          });
+
+          describe("When there are replies to comments", function() {
+            it("should nest the replies with the parent comment", function() {
+              var response = [
+                new Comment({ID: 1, content: 'Comment content 1', status: 'approved'}).toJSON(),
+                new Comment({ID: 2, content: 'Comment content 2', status: 'approved'}).toJSON(),
+                new Comment({ID: 3, content: 'Comment 1 reply 1', status: 'approved', parent: 1}).toJSON(),
+                new Comment({ID: 4, content: 'Comment 1 reply 2', status: 'approved', parent: 1}).toJSON(),
+                new Comment({ID: 5, content: 'Comment 2 reply 2', status: 'approved', parent: 2}).toJSON(),
+                new Comment({ID: 6, content: 'Reply 1 reply 1', status: 'approved', parent: 3}).toJSON(),
+                new Comment({ID: 7, content: 'Reply 2 reply 1', status: 'approved', parent: 3}).toJSON(),
+                new Comment({ID: 8, content: 'Reply 1 reply 2', status: 'approved', parent: 5}).toJSON()
+              ];
+
+              this.server = sinon.fakeServer.create();
+              this.server.respondWith(
+                'GET',
+                this.url + '/',
+                [200, {'Content-Type': 'application/json'}, JSON.stringify(response)]
+              );
+
+              this.view = new ContentSingleView({model: this.post, collection: new Comments()});
+              this.view.render();
+
+              this.server.respond();
+              expect(this.view.$('.b3-comments').children().length).toEqual(8);
+              expect(this.view.$('.b3-comment-content').length).toEqual(8);
+
+              expect(this.view.$('#comment-1 > ul.b3-comments').children().length).toEqual(2);
+              expect(this.view.$('#comment-2 > ul.b3-comments').children().length).toEqual(1);
+
+              expect(this.view.$('#comment-3 > ul.b3-comments').children().length).toEqual(2);
+              expect(this.view.$('#comment-5 > ul.b3-comments').children().length).toEqual(1);
+            });
           });
         });
 
