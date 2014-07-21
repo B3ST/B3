97c3364555ffffaea694a98e4226a0a0d133d771
Luis Rodrigues <luis.rodrigues@log.pt>
2014-07-22 00:13:56 +0100

    PHPUnit bootstrap

---
+++ test/phpunit/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Bootstrap the plugin unit testing environment.
+ *
+ * @package B3
+ * @subpackage B3/API
+ */
+
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS['wp_tests_options'] = array(
+    // 'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/plugin.php' ),
+);
+
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
+
+if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+    require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+} else {
+    require '../../../../../tests/phpunit/includes/bootstrap.php';
+}
+
+// Helper classes
+require_once dirname( __FILE__ ) . '/test_json_testcase.php';
