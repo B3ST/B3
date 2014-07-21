495648bdf3614dcf9d29b6ddab6860578760dcee
pcruz <pedro.cruz@log.pt>
2014-07-21 19:03:54 +0100

    Add support for replies in comments

--- app/views/comment-view.js
+++ app/views/comment-view.js
@@ -6,14 +6,28 @@ define([
   'dust',
   'dust.helpers',
   'dust.marionette',
+  'views/replyable-view',
   'content/comments/comment-template'
-], function ($, Marionette, dust, dustHelpers, dustMarionette) {
-  var CommentView = Backbone.Marionette.ItemView.extend({
+], function ($, Marionette, dust, dustHelpers, dustMarionette, ReplyableView) {
+  var view = _.extend(ReplyableView, {
     template: 'content/comments/comment-template.dust',
     tagName:  function () {
       return 'li id="comment-' + this.model.get('ID') + '"';
+    },
+
+    initialize: function (options) {
+      this.post = null;
+    },
+
+    events: {
+      'click a.b3-reply-comment': 'renderReplyBox',
+    },
+
+    parentId: function () {
+      return this.model.get('ID');
     }
   });
 
+  var CommentView = Backbone.Marionette.ItemView.extend(view);
   return CommentView;
 });
--- test/jasmine/specs/core/views/comment-view.spec.js
+++ test/jasmine/specs/core/views/comment-view.spec.js
@@ -1,12 +1,16 @@
 define([
   'views/comment-view',
   'models/comment-model',
-  'models/user-model'
-], function (CommentView, Comment, User) {
+  'models/post-model',
+  'models/user-model',
+  'views/reply-form-view'
+], function (CommentView, Comment, Post, User, ReplyFormView) {
   describe("CommentView", function() {
     beforeEach(function() {
+      this.post = new Post({ID: 1});
       this.comment = new Comment({
         ID:      1,
+        post:    this.post,
         content: 'Some Content',
         author:  new User({
           URL:  'user-url',
@@ -20,6 +24,19 @@ define([
       it("should render the template", function() {
         this.view.render();
         expect(this.view.el).toBeDefined();
+      });
+    });
+
+    describe("When clicking in reply", function() {
+      it("should display a comment box", function() {
+        this.view = new CommentView({model: this.comment});
+        this.view.render();
+
+        this.view.$('a.b3-reply-comment').click();
+
+        var template = new ReplyFormView({parentView: this.view, parentId: this.comment.get('ID')}).render().el;
+        var box      = this.view.$('.b3-reply-section').children()[0];
+        expect(box.isEqualNode(template)).toBeTruthy();
       });
     });
   });
