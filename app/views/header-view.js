d1fcae5ea33ef80524c4b62660138afa2ab2ac55
pcruz <pedro.cruz@log.pt>
2014-07-24 18:30:38 +0100

    Add primary menu to header

--- app/views/header-view.js
+++ app/views/header-view.js
@@ -1,28 +1,73 @@
 define([
   'jquery',
+  'underscore',
   'backbone',
   'dust',
   'dust.marionette',
   'models/settings-model',
+  'models/menu-model',
   'controllers/event-bus',
+  'controllers/navigator',
+  'content/content-template',
   'header-template'
-], function ($, Backbone, dust, dustMarionette, Settings, EventBus) {
+], function ($, _, Backbone, dust, dustMarionette, Settings, Menu, EventBus, Navigator) {
   'use strict';
 
   var HeaderView = Backbone.Marionette.ItemView.extend({
-    template: 'header-template.dust',
+    template: 'content/content-template.dust',
     tagName:  'div',
     events: {
-      'click #b3-header': 'index'
+      'click #b3-home': 'index',
+      'click .b3-menu': 'menu'
+    },
+
+    modelEvents: {
+      'change': 'render'
+    },
+
+    initialize: function (options) {
+      this.model = new Menu(options.menus.primary);
+      this.model.fetch();
     },
 
     serializeData: function () {
-      return Settings.attributes;
+      var items = {items: this.getItems()};
+      return _.extend(this.getDustTemplate(), items);
     },
 
-    index: function (e) {
-      EventBus.trigger('router:nav', {route: '', options: {trigger: true}});
-      return false;
+    index: function (ev) {
+      this.toggleActive(ev.currentTarget);
+      this.navigate('');
+      ev.preventDefault();
+    },
+
+    menu: function (ev) {
+      var link  = ev.currentTarget.href,
+          route = link.split(Settings.get('path') + '/')[1];
+
+      this.toggleActive(ev.currentTarget);
+      Navigator.navigate(route, true);
+      ev.preventDefault();
+    },
+
+    getItems: function () {
+      var menus = this.model.getItems();
+      return _.map(menus.models, function (menu) {
+        return menu.attributes;
+      });
+    },
+
+    getDustTemplate: function () {
+      return _.extend(Settings.attributes, {'parent-template': 'header-template.dust'});
+    },
+
+    toggleActive: function (link) {
+      this.$('.active').removeClass('active');
+      $(link).parent().addClass('active');
+    },
+
+    navigate: function (route) {
+      Navigator.navigate(route, true);
     }
   });
 
