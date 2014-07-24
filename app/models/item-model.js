dbf0f4bf9b7ef01fee8f2c8264af928b04425afb
pcruz <pedro.cruz@log.pt>
2014-07-24 18:24:54 +0100

    Add item model

---
+++ app/models/item-model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+define([
+  'jquery',
+  'backbone'
+], function ($, Backbone) {
+  var Item = Backbone.Model.extend({
+    defaults: {
+      ID:            null,
+      object:        null,
+      parent:        0,
+      order:         0,
+      object_parent: 0,
+      type:          '',
+      guid:          '',
+      object_type:   '',
+      link:          '',
+      title:         '',
+      attr_title:    '',
+      description:   '',
+      target:        '',
+      xfn:           '',
+      classes:       [],
+      meta:          {}
+    }
+  });
+
+  return Item;
+});
