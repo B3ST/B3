b51713f441b912ebbf54f4a8c1dee39e5f2d6cdf
pcruz <pedro.cruz@log.pt>
2014-07-28 13:31:47 +0100

    Add post filter

--- test/jasmine/specs/core/helpers/post-filter.spec.js
+++ test/jasmine/specs/core/helpers/post-filter.spec.js
@@ -6,34 +6,49 @@ define([
       this.filter = new PostFilter();
     });
 
+    it("should have serialize defined", function() {
+      expect(this.filter.serialize).toBeDefined();
+    });
+
     describe(".byCategory", function() {
-      // e.g.: decodeURIComponent($.param({filter: {category_name: 'sticky'}}))
-      it("should encode using the cat parameter", function() {
-        this.filter.byCategory();
-      });
-    });
-
-    describe(".byCategorySlug", function() {
       it("should encode using the category_name parameter", function() {
-        this.filter.byCategorySlug();
+        this.filter.byCategory('name');
+        expect(this.filter.serialize()).toEqual('filter[category_name]=name');
       });
     });
 
-    describe(".andCategory", function() {
-      it("should encode using the category__and parameter", function() {
-        this.filter.byCategorySlug();
+    describe(".byCategoryId", function() {
+      it("should encode using the cat parameter", function() {
+        this.filter.byCategoryId(1);
+        expect(this.filter.serialize()).toEqual('filter[cat]=1');
       });
     });
 
-    describe(".inCategory", function() {
-      it("should encode using the category__in parameter", function() {
-        this.filter.inCategory();
+    describe(".onPage", function() {
+      it("should encode using the page parameter", function() {
+        this.filter.onPage(1);
+        expect(this.filter.serialize()).toEqual('page=1');
       });
     });
 
-    describe(".notInCategory", function() {
-      it("should encode using the category__not parameter", function() {
-        this.filter.notInCategory();
+    describe(".perPage", function() {
+      it("should encode using the posts_per_page parameter", function() {
+        this.filter.perPage(10);
+        expect(this.filter.serialize()).toEqual('filter[posts_per_page]=10');
+      });
+    });
+
+    describe(".all", function() {
+      it("should encode using the posts_per_page parameter", function() {
+        this.filter.all();
+        expect(this.filter.serialize()).toEqual('filter[posts_per_page]=-1');
+      });
+    });
+
+    describe("When chaining methods", function() {
+      it("should return the parameters in the same query", function() {
+        this.filter.byCategory('name').onPage(1).perPage(10);
+        expect(this.filter.serialize()).toEqual('filter[category_name]=name&filter[posts_per_page]=10&page=1');
       });
     });
   });
