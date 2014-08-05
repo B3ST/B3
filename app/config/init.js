9eac76238e17fe94933f2d79f7dc9ab0e451d187
pcruz <pedro.cruz@log.pt>
2014-08-05 13:03:03 +0100

    Minor code presentation formatting

--- app/config/init.js
+++ app/config/init.js
@@ -104,13 +104,10 @@
 
     if (!String.prototype.supplant) {
       String.prototype.supplant = function(o) {
-        return this.replace(
-          /\{([^{}]*)\}/g,
-          function(a, b) {
-            var r = o[b];
-            return typeof r === 'string' || typeof r === 'number' ? r : a;
-          }
-        );
+        return this.replace(/\{([^{}]*)\}/g, function(a, b) {
+          var r = o[b];
+          return typeof r === 'string' || typeof r === 'number' ? r : a;
+        });
       };
     }
 
