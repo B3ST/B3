bea6887694c03ed395a67aab5286c3f75c0a4553
pcruz <pedro.cruz@log.pt>
2014-08-07 18:47:58 +0100

    Refactor search controller
    - Fetch the results and delegate them to archive controller
    - Trigger each event of the view
    - Navigate to the corresponding search url

--- test/jasmine/specs/core/controllers/search-controller.spec.js
+++ test/jasmine/specs/core/controllers/search-controller.spec.js
@@ -4,15 +4,15 @@ define([
   'app',
   'controllers/search-controller',
   'controllers/bus/event-bus',
+  'controllers/bus/command-bus',
   'models/settings-model',
   'models/post-model',
   'models/user-model',
   'collections/post-collection',
-  'views/empty-view',
   'views/archive-view',
   'views/not-found-view',
   'sinon'
-], function (App, SearchController, EventBus, Settings, Post, User, Posts, EmptyView, ArchiveView, NotFoundView) {
+], function (App, SearchController, EventBus, CommandBus, Settings, Post, User, Posts, ArchiveView, NotFoundView) {
   'use strict';
 
   function stubServer (options) {
@@ -28,78 +28,61 @@ define([
 
   describe("SearchController", function() {
     beforeEach(function() {
-      this.app  = App;
       this.user = new User({ID: 1, email: 'email', name: 'name'});
-      this.app.start();
-    });
-
-    afterEach(function() {
-      this.app.main.$el.html('');
-      this.app.header.$el.html('');
-      this.app.footer.$el.html('');
-      window.scrollTo(0, 0);
     });
 
     describe(".initialize", function() {
       beforeEach(function() {
         this.bus = spyOn(EventBus, 'bind');
-        this.controller = new SearchController({});
+        this.controller = new SearchController({ app: App });
       });
 
-      it("should bind to search:start event", function() {
-        expect(this.bus).toHaveBeenCalledWith('search:start', this.controller.showEmptySearchView);
+      it("should bind to search:view:start event", function() {
+        expect(this.bus).toHaveBeenCalledWith('search:view:start', this.controller.searchStart);
       });
 
-      it("should bind to search:term event", function() {
-        expect(this.bus).toHaveBeenCalledWith('search:term', this.controller.showSearchResults);
+      it("should bind to search:view:term event", function() {
+        expect(this.bus).toHaveBeenCalledWith('search:view:term', this.controller.showSearchResults);
       });
 
-      it("should bind to search:end event", function() {
-        expect(this.bus).toHaveBeenCalledWith('search:end', this.controller.showPreviousView);
+      it("should bind to search:view:submit event", function() {
+        expect(this.bus).toHaveBeenCalledWith('search:view:submit', this.controller.displaySearchUrl);
+      });
+
+      it("should bind to search:view:stop event", function() {
+        expect(this.bus).toHaveBeenCalledWith('search:view:stop', this.controller.searchStop);
       });
     });
 
-    describe(".showEmptySearchView", function() {
+    describe(".searchStart", function() {
       beforeEach(function() {
-        this.spy        = spyOn(this.app.main, 'show');
-        this.controller = new SearchController({
-          posts: new Posts(),
-          app:   this.app,
-          user:  this.user
-        });
-
-        this.controller.show(this.controller.archiveView(new Posts(), 1, null));
+        this.bus        = spyOn(EventBus, 'trigger');
+        this.command    = spyOn(CommandBus, 'execute');
+        this.controller = new SearchController({ app: App });
+        this.controller.searchStart();
       });
 
-      it("should display a blank empty view", function() {
-        this.controller.showEmptySearchView();
-
-        var view = this.spy.mostRecentCall.args[0];
-        expect(view instanceof EmptyView).toBeTruthy();
+      it("should trigger an event of search:start", function() {
+        expect(this.bus).toHaveBeenCalledWith('search:start');
       });
 
-      it("should save the previous view", function() {
-        this.controller.showEmptySearchView();
-        expect(this.controller.previousView).toBeDefined();
+      it("should trigger a command to display loading", function() {
+        expect(this.command).toHaveBeenCalledWith('loading:show', {region: App.main});
       });
     });
 
     describe(".showSearchResults", function() {
       it("should query with the given term", function() {
         this.fetch      = spyOn(Posts.prototype, 'fetch').andCallThrough();
-        this.controller = new SearchController({
-          posts: new Posts(),
-          app:   this.app,
-          user:  this.user
-        });
+        this.controller = new SearchController({ app: App });
 
         this.controller.showSearchResults({s: 'term'});
         expect(this.fetch).toHaveBeenCalledWith({reset: true, data: 'filter[s]=term&page=1'});
       });
 
       describe("When fetching is successful", function() {
-        it("should display the given results", function() {
-          this.spy = spyOn(this.app.main, 'show');
+        it("should trigger a search:results event with the obtained results", function() {
+          this.bus = spyOn(EventBus, 'trigger');
           var response = [
             new Post({ID: 1, title: 'post-1'}).toJSON(),
             new Post({ID: 2, title: 'post-2'}).toJSON()
@@ -109,40 +92,30 @@ define([
             url:      Settings.get('apiUrl') + '/posts?filter[s]=term&page=1',
             code:     200
           });
-          this.controller = new SearchController({
-            posts: new Posts(),
-            app:   this.app,
-            user:  this.user
-          });
+          this.controller = new SearchController({ app: App });
 
           this.controller.showSearchResults({s: 'term'});
           this.server.respond();
 
-          var view = this.spy.mostRecentCall.args[0];
-          expect(view instanceof ArchiveView).toBeTruthy();
+          expect(this.bus).toHaveBeenCalledWith('search:results:found', {results: jasmine.any(Posts), filter: jasmine.any(Object)});
         });
       });
 
       describe("When fetching fails", function() {
         it("should display a not found view", function() {
-          this.spy = spyOn(this.app.main, 'show');
+          this.bus = spyOn(EventBus, 'trigger');
           this.server = stubServer({
             response: '',
             url:      Settings.get('apiUrl') + '/posts?filter[s]=term&page=1',
             code:     404
           });
 
-          this.controller = new SearchController({
-            posts: new Posts(),
-            app:   this.app,
-            user:  this.user
-          });
+          this.controller = new SearchController({ app: App });
 
           this.controller.showSearchResults({s: 'term'});
           this.server.respond();
 
-          var view = this.spy.mostRecentCall.args[0];
-          expect(view instanceof NotFoundView).toBeTruthy();
+          expect(this.bus).toHaveBeenCalledWith('search:results:not_found');
         });
       });
     });
@@ -150,33 +123,30 @@ define([
     describe(".showSearch", function() {
       it("should display the results of the given query", function() {
         this.spy = spyOn(SearchController.prototype, 'showSearchResults');
-        this.controller = new SearchController({
-          posts: new Posts(),
-          app:   this.app,
-          user:  this.user
-        });
+        this.controller = new SearchController({ app: App });
 
-        this.controller.showSearch('s=term&page=2');
+        this.controller.showSearch({search: 'term', paged: 2});
         expect(this.spy).toHaveBeenCalledWith({s: 'term', page: 2});
       });
     });
 
-    describe(".showPreviousView", function() {
+    describe(".displaySearchUrl", function() {
+      it("should trigger a router:nav event to the corresponding url", function() {
+        this.bus        = spyOn(EventBus, 'trigger');
+        this.controller = new SearchController({ app: App });
+
+        this.controller.displaySearchUrl({s: 'result'});
+        EventBus.trigger('router:nav', {route: 'search/result', options: { trigger: false }});
+      });
+    });
+
+    describe(".searchStop", function() {
       it("should display the view prior to the search", function() {
-        this.spy        = spyOn(this.app.main, 'show');
-        this.controller = new SearchController({
-          posts: new Posts(),
-          app:   this.app,
-          user:  this.user
-        });
+        this.bus        = spyOn(EventBus, 'trigger');
+        this.controller = new SearchController({ app: App });
 
-        this.controller.show(this.controller.archiveView(new Posts(), 1, null));
-        this.controller.showEmptySearchView();
-        this.controller.showPreviousView();
-
-        var view = this.spy.mostRecentCall.args[0];
-        expect(view).toBeDefined();
-        expect(view).not.toBeNull();
+        this.controller.searchStop();
+        expect(this.bus).toHaveBeenCalledWith('search:stop');
       });
     });
   });
