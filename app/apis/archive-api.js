6455245f0daa2012010c934fc9f57c751d750e2c
pcruz <pedro.cruz@log.pt>
2014-09-25 16:26:50 +0100

    Add route handler for tag and category

--- app/apis/archive-api.js
+++ app/apis/archive-api.js
@@ -4,34 +4,43 @@ define([
   'backbone',
   'marionette',
   'controllers/archive-controller',
+  'buses/event-bus',
+  'models/settings-model',
   'helpers/post-filter'
-], function (Backbone, Marionette, ArchiveController, PostFilter) {
+], function (Backbone, Marionette, ArchiveController, EventBus, Settings, PostFilter) {
   'use strict';
 
   var ArchiveAPI = Backbone.Marionette.Controller.extend({
-    initialize: function (options) {
-      this.page   = options.page || 1;
-      this.filter = options.filter || new PostFilter();
-    },
-
     showHome: function (params) {
-      this.showArchive(params || {});
+      var onFront = Settings.get('page_on_front');
+      if (onFront > 0) {
+        EventBus.trigger('page:show', {page: onFront});
+      } else {
+        this.showArchive(params);
+      }
     },
 
     showArchive: function (params) {
-      this.taxonomy = null;
-      this.page     = params.paged || 1;
-      this.filter   = new PostFilter();
+      var page   = parseInt(params.paged, 10) || 1,
+          filter = new PostFilter().onPage(page);
 
-      new ArchiveController({ page: this.page, filter: this.filter }).showArchive();
+      this._showArchive(page, filter);
     },
 
-    showPostByCategory: function () {
+    showPostByCategory: function (params) {
+      var page   = parseInt(params.pages, 10) || 1,
+          catg   = params.category,
+          filter = new PostFilter().byCategory(catg).onPage(page);
 
+      this._showArchive(page, filter);
     },
 
-    showPostByTag: function () {
+    showPostByTag: function (params) {
+      var page   = parseInt(params.pages, 10) || 1,
+          ptag   = params.category,
+          filter = new PostFilter().byTag(ptag).onPage(page);
 
+      this._showArchive(page, filter);
     },
 
     showPostByAuthor: function  () {
@@ -44,6 +53,10 @@ define([
 
     showCustomTaxonomy: function  () {
 
+    },
+
+    _showArchive: function (page, filter) {
+      new ArchiveController({ page: page, filter: filter }).showArchive();
     }
   });
 
--- test/jasmine/specs/core/apis/archive-api.spec.js
+++ test/jasmine/specs/core/apis/archive-api.spec.js
@@ -2,22 +2,67 @@
 
 define([
   'apis/archive-api',
-  'controllers/archive-controller'
-], function (ArchiveAPI, ArchiveController) {
+  'controllers/archive-controller',
+  'models/settings-model',
+  'buses/event-bus'
+], function (ArchiveAPI, ArchiveController, Settings, EventBus) {
   'use strict';
 
   describe("ArchiveAPI", function() {
     var api;
 
+    describe(".showHome", function() {
+      describe("When home is not a page", function() {
+        it("should display the archive", function() {
+          spyOn(Settings, 'get').and.callFake(function () {
+            return 0;
+          });
+          var archive = spyOn(ArchiveAPI.prototype, 'showArchive');
+
+          api = new ArchiveAPI();
+          api.showHome();
+
+          expect(archive).toHaveBeenCalled();
+        });
+      });
+
+      describe("When home is set to a page", function() {
+        it("should display that page", function() {
+          spyOn(Settings, 'get').and.callFake(function () {
+            return 100;
+          });
+          var bus = spyOn(EventBus, 'trigger');
+
+          api = new ArchiveAPI();
+          api.showHome();
+
+          expect(bus).toHaveBeenCalledWith('page:show', {page: 100});
+        });
+      });
+    });
+
+    sharedArchiveAPIBehaviourFor(".showArchive", {
+      runTest: function (api) {
+        api.showArchive({});
+      }
+    });
+
+    sharedArchiveAPIBehaviourFor(".showPostByCategory", {
+      runTest: function (api) {
+        api.showPostByCategory({ category: 'category' });
+      }
+    })
+  });
+
+  function sharedArchiveAPIBehaviourFor (method, options) {
     describe(".showArchive", function() {
       it("should call showArchive from ArchiveController", function() {
-        var show = spyOn(ArchiveController.prototype, 'showArchive');
+        var show = spyOn(ArchiveController.prototype, 'showArchive'),
+            api  = new ArchiveAPI();
 
-        api = new ArchiveAPI();
-        api.showArchive({});
-
+        options.runTest(api);
         expect(show).toHaveBeenCalled();
       });
     });
-  });
+  }
 });
