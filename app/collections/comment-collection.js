35c79516c6729fc71e8a8dcf339b2c83908134fa
pcruz <pedro.cruz@log.pt>
2014-09-26 16:26:16 +0100

    Inject URL for comment collection

--- app/collections/comment-collection.js
+++ app/collections/comment-collection.js
@@ -7,7 +7,16 @@ define([
 ], function ($, Backbone, Comment) {
   'use strict';
   var Comments = Backbone.Collection.extend({
-    model:      Comment,
+    model: Comment,
+    url: function () {
+      return this.uri;
+    },
+
+    initialize: function (options) {
+      options = options || {};
+      this.uri = options.uri || '';
+    },
+
     comparator: function (comment) {
       return comment.get('ID');
     }
