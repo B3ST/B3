92c8003b435f3bddb10bda1e8de2eca6bf3e40ed
pcruz <pedro.cruz@log.pt>
2014-10-01 18:13:08 +0100

    Add search API

---
+++ app/apis/search-api.js
@@ -0,0 +1,16 @@
+/* global define */
+
+define([
+  'backbone',
+  'marionette'
+], function (Backbone, Marionette) {
+  'use strict';
+
+  var SearchAPI = Backbone.Marionette.Controller.extend({
+    showSearch: function () {
+
+    }
+  });
+
+  return SearchAPI;
+});
