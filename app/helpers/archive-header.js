4b6ef63ee78216da92cfbd5942e0ab524d52dc0c
pcruz <pedro.cruz@log.pt>
2014-10-13 15:52:02 +0100

    Support dates in archive header and template

--- app/helpers/archive-header.js
+++ app/helpers/archive-header.js
@@ -1,33 +1,60 @@
 /* global define */
 
 define([
-
-], function () {
+  'underscore',
+  'moment'
+], function (_, moment) {
   'use strict';
 
-  var ArchiveHeader = function () {};
+  var mapParameter = {
+    year:     'YYYY',
+    monthnum: 'MMMM',
+    day:      'Do'
+  }, ArchiveHeader = function () {};
+
+  function getDate (options) {
+    return {
+      years:  parseInt(options.year, 10),
+      months: parseInt(options.monthnum, 10) - 1,
+      days:   parseInt(options.day, 10)
+    };
+  }
+
+  function convertDate (options) {
+    return _.omit(getDate(options), function (value) {
+      return isNaN(value);
+    });
+  }
+
+  function getFormat (options) {
+    return _(_.keys(options)).reverse().map(function (value) {
+      return mapParameter.hasOwnProperty(value) ? mapParameter[value] : null;
+    }).compact().join(' ');
+  }
 
   ArchiveHeader.prototype = {
     archiveType: {
       taxonomy: function (posts, options) {
-        return posts.at(0).getTerm(options.taxonomy, options.term);
+        return posts.at(0).getTerm(options.taxonomy, options.term).toJSON();
       },
 
       author: function (posts) {
-        return posts.at(0).get('author');
+        return posts.at(0).get('author').toJSON();
       },
 
-      date: function () {
-        return new Date();
+      date: function (posts, options) {
+        var date = convertDate(options.date);
+        return { date: moment(date).format(getFormat(options.date)) };
       }
     },
 
     archiveBy: function (posts, options) {
+      options = options || {};
       if (options.archiveBy && this.archiveType.hasOwnProperty(options.archiveBy)) {
         return this.archiveType[options.archiveBy](posts, options);
       }
 
-      return null;
+      return false;
     }
   };
 
