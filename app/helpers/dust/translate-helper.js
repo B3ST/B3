7f684bbbc8b2413ad45723a86b469a321cdbabf7
pcruz <pedro.cruz@log.pt>
2014-10-14 12:51:22 +0100

    Add i18n helper

---
+++ app/helpers/dust/translate-helper.js
@@ -0,0 +1,28 @@
+/* global define */
+
+define([
+  'underscore',
+  'dust',
+  'nls/nls',
+  'dust.helpers'
+], function (_, dust, nls) {
+  'use strict';
+
+  dust.helpers.nls = function(chunk, context, bodies, params) {
+    var key = dust.helpers.tap(params.key, chunk, context);
+    return nls.i18n(key);
+  };
+
+  dust.helpers.section = function (chunk, context, bodies, params) {
+    var key    = dust.helpers.tap(params.key, chunk, context),
+        values = nls.i18n(key),
+        trns   = {};
+
+    _(values.length).times(function (n) {
+      trns['nls' + (n + 1)] = values[n];
+    });
+
+    chunk = chunk.render(bodies.block, context.push(trns));
+    return chunk;
+  };
+});
