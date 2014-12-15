de5807403523ee809e9fb1fd3b9ef0a7915bdede
pcruz <pedro.cruz@log.pt>
2014-12-15 17:57:28 +0000

    Add heartbeat comments fetcher

---
+++ inc/class-heartbeat-comments.php
@@ -0,0 +1,31 @@
+<?php
+
+class B3_Heartbeat_Comments {
+
+    public function get_last_updated() {
+        $comments = get_comments(
+            array(
+                'date_query' => $this->get_date_query()
+            )
+        );
+
+        return $this->filter_comments($comments);
+    }
+
+    private function get_date_query() {
+        return array(
+            array(
+                'after' => '15 minutes ago',
+            )
+        );
+    }
+
+    private function filter_comments( $comments ) {
+        $result = array();
+        foreach ($comments as $comment) {
+            array_push($result, $comment->comment_ID);
+        }
+
+        return $result;
+    }
+}
