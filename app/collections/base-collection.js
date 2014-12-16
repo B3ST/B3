ea6ebaa9ccfa983bbbb4977bc29c7062016ef408
pcruz <pedro.cruz@log.pt>
2014-12-16 11:55:31 +0000

    Add base collection to handle hearbeats

---
+++ app/collections/base-collection.js
@@ -0,0 +1,37 @@
+/* global define */
+
+define([
+  'underscore',
+  'backbone',
+  'buses/event-bus'
+], function (_, Backbone, EventBus) {
+  'use strict';
+
+  var BaseCollection = Backbone.Collection.extend({
+    constructor: function () {
+      Backbone.Collection.prototype.constructor.apply(this, arguments);
+      if (this.heartbeat) {
+        EventBus.on(this.heartbeat, this.onHeartbeat, this);
+      }
+    },
+
+    onHeartbeat: function (options) {
+      if (!this.hasAll(options)) {
+        this.fetch({ reset: true });
+        console.log('refreshing');
+      }
+    },
+
+    dismissHeartbeat: function () {
+      EventBus.off(this.heartbeat, this.onHeartbeat, this);
+    },
+
+    hasAll: function (values) {
+      return _.all(values, function (value) {
+        return !_.isEmpty(this.findWhere({ ID: value }));
+      }.bind(this));
+    }
+  });
+
+  return BaseCollection;
+});
