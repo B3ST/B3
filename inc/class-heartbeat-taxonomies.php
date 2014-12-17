11f9190dcf16569c2f8e3a017b245abd7655d63c
pcruz <pedro.cruz@log.pt>
2014-12-17 10:40:36 +0000

    Merge instead of push

--- inc/class-heartbeat-taxonomies.php
+++ inc/class-heartbeat-taxonomies.php
@@ -4,12 +4,12 @@ class B3_Heartbeat_Taxonomies {
     public function get_taxonomies( $posts ) {
         $result = array();
         foreach ( $posts as $post ) {
-            $id    = $post->ID;
-            $types = get_object_taxonomies( get_post_type( $id ) );
-            $terms = wp_get_post_terms( $id, $types, array( 'fields' => 'ids' ) );
-            array_push( $result, $terms );
+            $id     = $post->ID;
+            $types  = get_object_taxonomies( get_post_type( $id ) );
+            $terms  = wp_get_post_terms( $id, $types, array( 'fields' => 'ids' ) );
+            $result = array_merge( $result, $terms );
         }
 
-        return $result;
+        return array_unique( $result );
     }
 }
