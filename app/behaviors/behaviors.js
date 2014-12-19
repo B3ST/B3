502c7310d7b597ad510560103d4885a4d54a1479
pcruz <pedro.cruz@log.pt>
2014-12-19 18:52:59 +0000

    Add behaviors support

---
+++ app/behaviors/behaviors.js
@@ -0,0 +1,13 @@
+/* global define */
+
+define([
+  'backbone',
+  'marionette'
+], function (Backbone, Marionette) {
+  'use strict';
+
+  window.Behaviors = {};
+  Marionette.Behaviors.behaviorsLookup = function() {
+    return window.Behaviors;
+  };
+});
