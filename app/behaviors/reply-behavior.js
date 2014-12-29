4fd638f31f6483bbf1aa5bb1a19f7f7575ac8a66
pcruz <pedro.cruz@log.pt>
2014-12-29 17:17:49 +0000

    Replace ReplyableView with a Reply behaviour

---
+++ app/behaviors/reply-behavior.js
@@ -0,0 +1,123 @@
+/* global define */
+
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'marionette',
+  'buses/event-bus',
+  'buses/command-bus',
+  'models/settings-model'
+], function ($, _, Backbone, Marionette, EventBus, CommandBus, Settings) {
+  'use strict';
+
+  var Reply = Marionette.Behavior.extend({
+    events: {
+      'click @ui.replyButton':  'displayReplyForm',
+      'click @ui.submitButton': 'onSubmitReply',
+      'submit @ui.replyForm':   'onSubmitReply'
+    },
+
+    displayReplyForm: function (ev) {
+      var replyButton = $(ev.currentTarget),
+          id     = _.uniqueId('reply-form-container'),
+          region = new Marionette.Region({ el: '#' + id });
+
+      if (this.$('.reply-form').length === 0) {
+        this._displayReplyForm(replyButton, id, region);
+        this._bindToEvents();
+        this.triggerMethod('reply:clicked');
+      }
+
+      return false;
+    },
+
+    showButton: function (options) {
+      if (options.id === this._parentId()) {
+        this.button.fadeIn('fast');
+        this.$('.reply-form').remove();
+        EventBus.off('replyform:cancel', this.showButton, this);
+      }
+    },
+
+    onSubmitReply: function (ev) {
+      ev.preventDefault();
+
+      if (this._validateForm()) {
+        EventBus.trigger('replyform:view:reply', this._getComment());
+      }
+    },
+
+    _validateForm: function () {
+      var valid = this._validateFormGroup();
+
+      if (!valid) {
+        this.view.displayWarning('Please fill all required fields.');
+      } else {
+        this.view.resetWarning();
+      }
+
+      return valid;
+    },
+
+    _validateFormGroup: function () {
+      var valid = true;
+
+      this.$('.form-group').each(function (index, group) {
+        $(group).removeClass('has-error');
+
+        $(group).find('.required').each(function(index, input) {
+          if (_.isEmpty($(input).val())) {
+            $(group).addClass('has-error');
+            valid = false;
+          }
+        });
+      }.bind(this));
+
+      return valid;
+    },
+
+    _getComment: function () {
+      return {
+        content:        this.$('[name="comment_content"]').val(),
+        post:           this.view.post.get('ID'),
+        parent_comment: this.view.parentId,
+        author:         this.view.getUser()
+      };
+    },
+
+    _displayReplyForm: function (button, id, region) {
+      this.button = button;
+      $(this.button).after('<div id="' + id + '" class="reply-form"></div>');
+      CommandBus.execute('reply:form:show', this._getOptions(region));
+      $(this.button).fadeOut('fast');
+    },
+
+    _bindToEvents: function () {
+      EventBus.on('replyform:cancel', this.showButton, this);
+    },
+
+    _getOptions: function (region) {
+      return {
+        region:     region,
+        post:       this._post(),
+        user:       Settings.get('me'),
+        parentView: this,
+        parentId:   this._parentId()
+      };
+    },
+
+    _post: function () {
+      return _.isFunction(this.view.getPost) ? this.view.getPost() : this.view.model;
+    },
+
+    _parentId: function () {
+      return _.isFunction(this.view.parentId) ? this.view.parentId() : 0;
+    }
+
+  });
+
+  window.Behaviors.Reply = Reply;
+
+  return Reply;
+});
