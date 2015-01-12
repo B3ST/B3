8d85f252e3fe6d737996d5dc76b3cb28b7fd09f2
pcruz <pedro.cruz@log.pt>
2015-01-12 14:40:31 +0000

    Fix single post view specs

--- test/jasmine/specs/core/views/single-post-view.spec.js
+++ test/jasmine/specs/core/views/single-post-view.spec.js
@@ -1,101 +1,67 @@
 /* global define, sinon, describe, beforeEach, expect, it, spyOn */
 
 define([
-  'jquery',
   'views/single-post-view',
   'buses/event-bus',
-  'views/reply-form-view',
+  'buses/navigator',
   'models/user-model',
   'models/post-model',
-  'models/comment-model',
-  'collections/comment-collection',
-  'sinon'
-], function ($, SinglePostView, EventBus, ReplyFormView, User, Post, Comment, Comments) {
+], function (SinglePostView, EventBus, Navigator, User, Post) {
   'use strict';
 
-  describe("SinglePostView", function() {
-    var user, view, post, options
+  describe('SinglePostView', function() {
+    var user, view, post, options;
 
     beforeEach(function() {
-      post    = new Post({ID: 1, title: 'Title', author: new User({ID: 1, slug: 'author-1', name: 'author-name'}), terms: {category: {ID: 1, slug: 'post-1', link: "http://localhost:8888/wordpress/post/category/content"}, post_tag: {ID: 1, slug: 'tag-1', link: "http://localhost:8888/wordpress/post/tag/content"}}});
+      post    = new Post({ID: 1, title: 'Title', content: '<a href="http://wordpress.example.org/post/category/content">link</a>', author: new User({ID: 1, slug: 'author-1', name: 'author-name'}), terms: {portfolio: [{ ID: 1, slug: 'post-1', link: 'http://wordpress.example.org/post/portfolio/content' }], category: {ID: 1, slug: 'post-1', link: 'http://wordpress.example.org/post/category/content'}, post_tag: {ID: 1, slug: 'tag-1', link: 'http://wordpress.example.org/post/tag/content'}}});
       options = { model: post, template: 'content/type-post-template.dust' };
     });
 
-    describe(".initialize", function() {
+    describe('.initialize', function() {
       var bus;
 
       beforeEach(function() {
-        bus = spyOn(EventBus, 'trigger');
+        bus  = spyOn(EventBus, 'trigger');
         view = new SinglePostView(options);
       });
 
-      it("should set the document title", function () {
+      it('should set the document title', function () {
         expect(bus).toHaveBeenCalledWith('title:change', 'Title');
       });
     });
 
-    describe("When clicking in a link", function() {
-      it("should trigger a single:view:link:clicked event", function() {
-        var trigger = spyOn(EventBus, 'trigger'),
-            post    = new Post({ID: 1, title: 'Title', content: '<a href="link">link</a>', author: new User({ID: 1, slug: 'author-1', name: 'author-name'}), terms: {category: {ID: 1, slug: 'post-1', link: "http://localhost:8888/wordpress/post/category/content"}, post_tag: {ID: 1, slug: 'tag-1', link: "http://localhost:8888/wordpress/post/tag/content"}}}),
-            view    = new SinglePostView({ model: post, template: 'content/type-post-template.dust' }), link;
+    using('Single Post links',
+      [{ title: 'content', ui: '.post-content > a' },
+       { title: 'taxonomy', ui: '.taxonomy > a' },
+       { title: 'category', ui: '.category > a' },
+       { title: 'tag', ui: '.tag > a' }], function (data) {
+      describe('When clicking in a ' + data.title + ' link', function() {
+        it('should navigate to the given link', function() {
+          var navigate = spyOn(Navigator, 'navigateToLink'),
+              view    = new SinglePostView({ model: post, template: 'content/type-post-template.dust' }), link;
 
-        view.render();
-        link = view.$('.post-content > a');
-        link.click();
+          view.render();
+          link = view.$(data.ui).first();
+          link.click();
 
-        expect(trigger).toHaveBeenCalledWith('single:view:link:clicked', { href: link.attr('href') });
+          expect(navigate).toHaveBeenCalledWith(link.attr('href'), true);
+        });
       });
     });
 
-    describe("When clicking in a taxonomy link", function() {
-      it("should trigger a single:view:display:taxonomy event", function() {
-        var trigger = spyOn(EventBus, 'trigger'),
-            post    = new Post({ID: 1, title: 'Title', content: '<a href="link">link</a>', author: new User({ID: 1, slug: 'author-1', name: 'author-name'}), terms: {portfolio: [{ID: 1, slug: 'post-1', link: "http://localhost:8888/wordpress/post/portfolio/content"}], post_tag: {ID: 1, slug: 'tag-1', link: "http://localhost:8888/wordpress/post/tag/content"}}}),
-            view    = new SinglePostView({ model: post, template: 'content/type-post-template.dust' }), link;
+    using('Author links', ['.author > a', '#author > a'], function (link) {
+      describe('When clicking in an author link', function() {
+        it('should navigate to that link', function() {
+          var navigate = spyOn(Navigator, 'navigateToAuthor'),
+              view     = new SinglePostView({ model: post, template: 'content/type-post-template.dust' });
 
-        view.render();
-        link = view.$('.taxonomy > a');
-        link.click();
+          view.render();
+          link = view.$(link).first();
+          link.click();
 
-        expect(trigger).toHaveBeenCalledWith('single:view:display:taxonomy', { href: link.attr('href') });
+          expect(navigate).toHaveBeenCalledWith('author-1', 1, true);
+        });
       });
     });
-
-    sharedClickBehaviourFor('category', {
-      click: '.category > a',
-      route: 'post/category/post-1'
-    });
-
-    sharedClickBehaviourFor('post_tag', {
-      click: '.tag > a',
-      route: 'post/tag/tag-1'
-    });
-
-    sharedClickBehaviourFor('author', {
-      click: '.author > a',
-      route: 'post/author/author-1'
-    });
-
-    sharedClickBehaviourFor('author', {
-      click: '#author > a',
-      route: 'post/author/author-1'
-    })
   });
-
-  function sharedClickBehaviourFor (action, options) {
-    describe("When clicking in " + action, function() {
-      it("should trigger a single:display:" + action, function() {
-        var trigger = spyOn(EventBus, 'trigger'),
-            post    = new Post({ID: 1, title: 'Title', author: new User({ID: 1, slug: 'author-1', name: 'author-name'}), terms: {category: {ID: 1, slug: 'post-1', link: "http://localhost:8888/wordpress/post/category/content"}, post_tag: {ID: 1, slug: 'tag-1', link: "http://localhost:8888/wordpress/post/tag/content"}}}),
-            view    = new SinglePostView({ model: post, template: 'content/type-post-template.dust' }), button;
-
-        view.render();
-        button = view.$(options.click);
-        button.click();
-
-        expect(trigger).toHaveBeenCalledWith('single:view:display:' + action, { slug: button.attr('slug'), type: action });
-      });
-    });
-  }
 });
