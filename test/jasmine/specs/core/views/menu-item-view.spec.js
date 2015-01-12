1f2e37645eecd383be8156b674d1af407eb9e4fe
pcruz <pedro.cruz@log.pt>
2015-01-12 14:59:10 +0000

    Fix menu item view spec

--- test/jasmine/specs/core/views/menu-item-view.spec.js
+++ test/jasmine/specs/core/views/menu-item-view.spec.js
@@ -9,66 +9,15 @@ define([
 ], function (MenuItemView, MenuItem, Settings, EventBus, Navigator) {
   'use strict';
 
-  describe("MenuItemView", function() {
+  describe('MenuItemView', function() {
     var item, view;
     beforeEach(function() {
-      var json = getJSONFixture("menu_item.json");
+      var json = getJSONFixture('menu_item.json');
       item = new MenuItem(json);
     });
 
-    describe(".initialize", function() {
-      it("should bind to a given set of events", function() {
-        var bus = spyOn(EventBus, 'on');
-        view = new MenuItemView({model: item});
-
-        expect(bus).toHaveBeenCalledWith('view:menu:activation', view.itemSelected, view);
-        expect(bus).toHaveBeenCalledWith('header:view:index', view.itemSelected, view);
-      });
-    });
-
-    describe(".itemSelected", function() {
-      beforeEach(function() {
-        view = new MenuItemView({model: item});
-        view.render();
-        view.$el.addClass('active');
-      });
-
-      it("should deactivate its menu if the sent id is not the same", function() {
-        view.itemSelected({id: 1});
-        expect(view.$el.attr('class')).not.toEqual('active');
-      });
-
-      describe("When the sent parent id is the same as the model id", function() {
-        it("should activate its menu", function() {
-          view.itemSelected({ id: 1130, parent: 1257 });
-          expect(view.$el.attr('class')).toEqual('active');
-        });
-
-        it("should trigger the same event setting its own parent", function() {
-          var trigger = spyOn(EventBus, 'trigger');
-          view.itemSelected({ id: 1130, parent: 1257 });
-          expect(trigger).toHaveBeenCalledWith('view:menu:activation', {id: 1130, parent: 0});
-        });
-      });
-    });
-
-    describe(".toggleDropdown", function() {
-      it("should turn the menu into a dropdown menu", function() {
-        view = new MenuItemView({ model: item });
-        view.render();
-        view.toggleDropdown();
-
-        expect(view.$el.attr('class')).toEqual('dropdown');
-        expect(view.$('.menu-item').attr('class')).toContain('dropdown-toggle');
-        expect(view.$('.menu-item').attr('data-toggle')).toEqual('dropdown');
-        expect(view.$('.menu-item').html()).toContain('<span class="caret"></span>');
-        expect(view.$('ul').attr('class')).toEqual('dropdown-menu');
-        expect(view.$('ul').attr('role')).toEqual('menu');
-      });
-    });
-
     using('menu types', ['page', 'post', 'category', 'tag'], function (type) {
-      describe("Clicking an item", function() {
+      describe('Clicking an item', function() {
         var trigger, navigate;
 
         beforeEach(function() {
@@ -86,48 +35,27 @@ define([
           view.render();
         });
 
-        it("should trigger menu selection events", function() {
+        it('should trigger menu selection events', function() {
           view.$('.menu-item').click();
-          expect(trigger).toHaveBeenCalledWith('view:menu:activation', {id: 1257, parent: 0});
+          expect(trigger).toHaveBeenCalledWith('change:menu:item:state', { id: 1257, parent: 0 });
         });
 
-        it("should trigger navigate event to the given link", function() {
-          view.$('.menu-item').click();
-          expect(trigger).toHaveBeenCalledWith('menu-item:view:navigate', { link: Settings.get('site_url') + type + 'about' });
-        });
-
-        it("should activate the menu item", function() {
+        it('should activate the menu item', function() {
           view.$('.menu-item').click();
           expect(view.$el.attr('class')).toContain('active');
         });
 
-        describe("when the link is for an external resource", function () {
+        describe('when the link is for an external resource', function () {
           beforeEach(function() {
             item.set({'link': 'http://log.pt/'});
             view = new MenuItemView({model: item});
             view.render();
           });
 
-          it("should not trigger menu selection events", function() {
+          it('should not trigger menu selection events', function() {
             view.$('.menu-item').click();
             type = type.replace('page', '');
-            expect(trigger).not.toHaveBeenCalledWith('router:nav', {route: type + "/about", options: {trigger: true}});
-          });
-        });
-
-        describe("when the menu is a dropdown", function() {
-          beforeEach(function() {
-            view.toggleDropdown();
-          });
-
-          it("should not trigger any events", function() {
-            view.$('.menu-item').click();
-            expect(trigger).not.toHaveBeenCalled();
-          });
-
-          it("should not activate the menu item", function() {
-            view.$('.menu-item').click();
-            expect(view.$el.attr('class')).not.toContain('active');
+            expect(trigger).not.toHaveBeenCalledWith('router:nav', {route: type + '/about', options: {trigger: true}});
           });
         });
       });
