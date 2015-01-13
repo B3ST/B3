9c9f55d65a93a1f544a0449a6eee17a925e1ee13
pcruz <pedro.cruz@log.pt>
2015-01-13 13:18:04 +0000

    Minor fixes

--- test/jasmine/specs/core/views/pagination-view.spec.js
+++ test/jasmine/specs/core/views/pagination-view.spec.js
@@ -6,10 +6,10 @@ define([
 ], function (PaginationView, EventBus) {
   'use strict';
 
-  describe("PaginationView", function() {
+  describe('PaginationView', function() {
     var view, bus;
 
-    describe("When clicking in next button", function() {
+    describe('When clicking in next button', function() {
       beforeEach(function() {
         bus  = spyOn(EventBus, 'trigger');
         view = new PaginationView({ pages: 2, page: 1 });
@@ -17,20 +17,20 @@ define([
         view.$('.next > a').click();
       });
 
-      it("should trigger a pagination:view:display:next:page event", function() {
-        expect(bus).toHaveBeenCalledWith('pagination:view:display:next:page', { page: 2 });
+      it('should trigger a pagination:next:page event', function() {
+        expect(bus).toHaveBeenCalledWith('pagination:next:page', { page: 2 });
       });
 
-      it("should increment the page", function() {
+      it('should increment the page', function() {
         expect(view.page).toEqual(2);
       });
 
-      it("should disable the next button if it is the last page", function() {
+      it('should disable the next button if it is the last page', function() {
         expect(view.$('.next').hasClass('disabled')).toBeTruthy();
       });
     });
 
-    describe("When clicking in previous button", function() {
+    describe('When clicking in previous button', function() {
       beforeEach(function() {
         bus  = spyOn(EventBus, 'trigger');
         view = new PaginationView({ pages: 2, page: 2 });
@@ -38,20 +38,20 @@ define([
         view.$('.previous > a').click();
       });
 
-      it("should trigger a pagination:view:display:previous:page event", function() {
-        expect(bus).toHaveBeenCalledWith('pagination:view:display:previous:page', { page: 1 });
+      it('should trigger a pagination:previous:page event', function() {
+        expect(bus).toHaveBeenCalledWith('pagination:previous:page', { page: 1 });
       });
 
-      it("should increment the page", function() {
+      it('should increment the page', function() {
         expect(view.page).toEqual(1);
       });
 
-      it("should disable the previous button if it is the last page", function() {
+      it('should disable the previous button if it is the last page', function() {
         expect(view.$('.previous').hasClass('disabled')).toBeTruthy();
       });
     });
 
-    describe("When clicking in a page button", function() {
+    describe('When clicking in a page button', function() {
       beforeEach(function() {
         bus  = spyOn(EventBus, 'trigger');
         view = new PaginationView({ include: true, pages: 2, page: 2 });
@@ -59,15 +59,15 @@ define([
         view.$('.pagination .number:eq(0) > a').click();
       });
 
-      it("should trigger a pagination:view:display:page event", function() {
-        expect(bus).toHaveBeenCalledWith('pagination:view:display:page', { page: 1 });
+      it('should trigger a pagination:select:page event', function() {
+        expect(bus).toHaveBeenCalledWith('pagination:select:page', { page: 1 });
       });
 
-      it("should increment the page", function() {
+      it('should increment the page', function() {
         expect(view.page).toEqual(1);
       });
 
-      it("should set the clicked page active", function() {
+      it('should set the clicked page active', function() {
         expect(view.$('.pagination .number:eq(0) > a').parent().hasClass('active')).toBeTruthy();
       });
     });
