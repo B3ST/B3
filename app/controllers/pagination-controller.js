50b02016d83e7e72f352ca3ea9ca826b4789dd64
pcruz <pedro.cruz@log.pt>
2015-01-13 13:17:41 +0000

    Remove unnecessary event overhead

--- app/controllers/pagination-controller.js
+++ app/controllers/pagination-controller.js
@@ -2,32 +2,13 @@
 
 define([
   'controllers/base-controller',
-  'views/pagination-view',
-  'buses/event-bus'
-], function (BaseController, PaginationView, EventBus) {
+  'views/pagination-view'
+], function (BaseController, PaginationView) {
   'use strict';
 
   var PaginationController = BaseController.extend({
-    busEvents: {
-      'pagination:view:display:next:page':     'nextPage',
-      'pagination:view:display:previous:page': 'previousPage',
-      'pagination:view:display:page':          'selectPage'
-    },
-
     showPagination: function (options) {
       this.show(new PaginationView(options), { region: options.region });
-    },
-
-    nextPage: function (options) {
-      EventBus.trigger('pagination:next:page', options);
-    },
-
-    previousPage: function (options) {
-      EventBus.trigger('pagination:previous:page', options);
-    },
-
-    selectPage: function (options) {
-      EventBus.trigger('pagination:select:page', options);
     }
   });
 
