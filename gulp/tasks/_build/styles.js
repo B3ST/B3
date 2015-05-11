29937cf7c04192e827af01238b7c3c735a6dbd90
Luis Rodrigues <hello@goblindegook.net>
2015-05-12 00:51:55 +0100

    Fixed Bootstrap's JS deps, added scss-lint

--- gulp/tasks/_build/styles.js
+++ gulp/tasks/_build/styles.js
@@ -1,31 +1,33 @@
 /* global require */
 
-"use strict";
+'use strict';
 
-var gulp         = require("gulp"),
-    browserSync  = require("browser-sync"),
-    autoprefixer = require("gulp-autoprefixer"),
-    concat       = require("gulp-concat"),
-    gulpIf       = require("gulp-if"),
-    sass         = require("gulp-sass"),
-    minifyCss    = require("gulp-minify-css"),
-    plumber      = require("gulp-plumber"),
-    size         = require("gulp-size"),
-    sourcemaps   = require("gulp-sourcemaps"),
-    handleErrors = require("../../util/handleErrors"),
-    config       = require("../../config");
+var gulp            = require('gulp'),
+    browserSync     = require('browser-sync'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    concat          = require('gulp-concat'),
+    gulpIf          = require('gulp-if'),
+    sass            = require('gulp-sass'),
+    scssLint        = require('gulp-scss-lint'),
+    scssLintStylish = require('gulp-scss-lint-stylish'),
+    minifyCss       = require('gulp-minify-css'),
+    plumber         = require('gulp-plumber'),
+    size            = require('gulp-size'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    handleErrors    = require('../../util/handleErrors'),
+    config          = require('../../config');
 
-gulp.task("build:styles", ["build:images"], function() {
+gulp.task('build:styles', ['build:images'], function() {
   return gulp.src(config.sass.src)
     .pipe(plumber())
+    .pipe(scssLint({customReport: scssLintStylish}))
     .pipe(gulpIf(config.debug, sourcemaps.init()))
-      .pipe(sass())
-        .on("error", handleErrors)
-      .pipe(autoprefixer(config.autoprefixer))
-      .pipe(minifyCss())
-      .pipe(concat(config.sass.concat))
-    .pipe(gulpIf(config.debug, sourcemaps.write(".")))
+    .pipe(sass(config.sass.settings))
+      .on('error', handleErrors)
+    .pipe(autoprefixer(config.autoprefixer))
+    .pipe(minifyCss({keepSpecialComments: '*'}))
+    .pipe(gulpIf(config.debug, sourcemaps.write()))
     .pipe(gulp.dest(config.sass.dest))
     .pipe(browserSync.reload({stream: true}))
-    .pipe(size({title: "build:styles"}));
+    .pipe(size({title: 'build:styles'}));
 });
--- gulp/tasks/bower.js
+++ gulp/tasks/bower.js
@@ -3,20 +3,13 @@
 "use strict";
 
 var gulp       = require("gulp"),
-    uglify     = require("gulp-uglify"),
-    minifyCss  = require("gulp-minify-css"),
     config     = require("../config"),
     bowerFiles = require('bower-files')({'dev': config.debug});
 
 gulp.task('bower', function () {
   gulp.src(bowerFiles.js)
-    .pipe(uglify())
     .pipe(gulp.dest(config.lib));
 
   gulp.src(bowerFiles.css)
-    .pipe(minifyCss())
     .pipe(gulp.dest(config.lib + "css/"));
-
-  gulp.src(bowerFiles.eot.concat(bowerFiles.svg).concat(bowerFiles.ttf).concat(bowerFiles.woff))
-    .pipe(gulp.dest(config.lib + "fonts/"));
 });
